4b014a7f536e9cf49f0147735c8db3fcbc62cdeb
Ryan Krug <ryan@elbongurk.com>
2013-06-26 15:23:42 -0500

    Make .yaml trigger yaml-mode

--- init.el
+++ init.el
@@ -56,6 +56,7 @@
 (autoload 'yaml-mode "yaml-mode" 
   "Major mode for editing YAML files" t)
 (add-to-list 'auto-mode-alist '("\\.yml" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml" . yaml-mode))
 
 ;;coffeescript
 (add-to-list 'load-path "~/.emacs.d/vendor/coffee-mode")
