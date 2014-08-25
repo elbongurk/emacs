4f88d545e8174b30c49c1a3aec4f7faec15c0834
keegnotrub <ryan@elbongurk.com>
2014-08-25 13:45:52 -0500

    Removing Ruby specific modules

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
 # Emacs Setup
 
-This is an Emacs setup for Ruby on Rails development. Includes modes synced to git repo's for the following:
+This is an Emacs setup for general development. Includes modes synced to git repo's for the following:
 
-* Ruby on Rails via [rinari][1]
-* ERB for (html) via [rhtml][2]
-* YAML via [yaml-mode][3]
-* CoffeeScript via [coffee-mode][4]
-* SCSS via [scss-mode][5]
-* Markdown via [markdown-mode][6]
-* Git via [magit][7]
+* Markdown via [markdown-mode][1]
+* Access to PATH on OS X via [exec-path-from-shell][2]
 
 ## Installation
 
@@ -16,10 +11,5 @@ This is an Emacs setup for Ruby on Rails development. Includes modes synced to g
     cd ~/.emacs.d
     git submodule update --init --recursive
 
-[1]: https://github.com/eschulte/rinari
-[2]: https://github.com/eschulte/rhtml
-[3]: https://github.com/yoshiki/yaml-mode
-[4]: https://github.com/defunkt/coffee-mode
-[5]: https://github.com/antonj/scss-mode
-[6]: http://jblevins.org/projects/markdown-mode/
-[7]: https://github.com/magit/magit
+[1]: http://jblevins.org/projects/markdown-mode/
+[2]: https://github.com/purcell/exec-path-from-shell
--- init.el
+++ init.el
@@ -5,10 +5,6 @@
 (setq inhibit-splash-screen t)
 (setq initial-scratch-message nil)
 
-;;dont wrap text except for text files but then wrap words
-(set-default 'truncate-lines t)
-(add-hook 'text-mode-hook 'turn-on-visual-line-mode)
-
 ;;remove unwanted ui
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
@@ -27,58 +23,6 @@
 (when (memq window-system '(mac ns))
   (require 'exec-path-from-shell)
   (exec-path-from-shell-initialize))
-
-;;magit
-(add-to-list 'load-path "~/.emacs.d/vendor/git-modes")
-(add-to-list 'load-path "~/.emacs.d/vendor/magit")
-(require 'magit)
-
-;;foreman
-(add-to-list 'load-path "~/.emacs.d/vendor/foreman.el")
-(require 'foreman)
-
-;;rinari
-(add-to-list 'load-path "~/.emacs.d/vendor/rinari")
-(require 'rinari)
-(global-rinari-mode)
-
-;;bundler
-(add-to-list 'load-path "~/.emacs.d/vendor/bundler.el")
-(require 'bundler)
-
-;;rhtml
-(add-to-list 'load-path "~/.emacs.d/vendor/rhtml")
-(autoload 'rhtml-mode "rhtml-mode" 
-  "Minor Mode for editing rhtml files" t)
-(add-to-list 'auto-mode-alist '("\\.html\\.erb" . rhtml-mode))
-(add-to-list 'auto-mode-alist '("\\.svg\\.erb" . rhtml-mode))
-
-;;ruby
-(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Procfile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.ru" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.gemspec" . ruby-mode))
-
-;;yaml
-(add-to-list 'load-path "~/.emacs.d/vendor/yaml-mode")
-(autoload 'yaml-mode "yaml-mode" 
-  "Major mode for editing YAML files" t)
-(add-to-list 'auto-mode-alist '("\\.yml" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.yaml" . yaml-mode))
-
-;;coffeescript
-(add-to-list 'load-path "~/.emacs.d/vendor/coffee-mode")
-(autoload 'coffee-mode "coffee-mode" 
-  "Major mode for editing CoffeeScript files" t)
-(add-to-list 'auto-mode-alist '("\\.coffee" . coffee-mode))
-
-;;scss
-(setq scss-compile-at-save nil)
-(add-to-list 'load-path "~/.emacs.d/vendor/scss-mode")
-(autoload 'scss-mode "scss-mode" 
-  "Major mode for editing SCSS files" t)
-(add-to-list 'auto-mode-alist '("\\.scss" . scss-mode))
 
 ;;markdown
 (add-to-list 'load-path "~/.emacs.d/vendor/markdown-mode")
