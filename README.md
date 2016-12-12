264835d7e40cd5dcf4043a3bc75db8e7c2ebd880
keegnotrub <keegnotrub@icloud.com>
2016-12-12 15:22:02 -0600

    Use Melpa

--- README.md
+++ README.md
@@ -1,15 +1,8 @@
 # Emacs Setup
 
-This is an Emacs setup for general development. Includes modes synced to git repo's for the following:
-
-* Markdown via [markdown-mode][1]
-* Access to PATH on OS X via [exec-path-from-shell][2]
+This is an Emacs setup for Ruby development.
 
 ## Installation
 
     git clone https://github.com/keegnotrub/emacs.git ~/.emacs.d
-    cd ~/.emacs.d
-    git submodule update --init --recursive
 
-[1]: http://jblevins.org/projects/markdown-mode/
-[2]: https://github.com/purcell/exec-path-from-shell
--- init.el
+++ init.el
@@ -1,38 +1,109 @@
 ;;save compulsively instead
 (setq make-backup-files nil)
 
-;;splash screen not needed and use blank scratch
+;;we hates splash
 (setq inhibit-splash-screen t)
 (setq initial-scratch-message nil)
 
-;;remove unwanted ui
-(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+;;we hates ui
+(unless (memq window-system '(mac ns))
+  (when (fboundp 'menu-bar-mode)
+    (menu-bar-mode -1)))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(when (fboundp 'tool-bar-mode)
+    (tool-bar-mode -1))
 
-;;set tab width globally
+;;we hates tabs
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
-(setq js-indent-level 2)
-(setq css-indent-offset 2)
 
-;;disable Emacs version control
-(setq vc-handled-backends nil)
+;;load melpa packages
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
 
-;;get the PATH variable right on Mac windowed Emacs
-(add-to-list 'load-path "~/.emacs.d/vendor/exec-path-from-shell")
+
+(setq package-list '(markdown-mode
+                     yaml-mode
+                     scss-mode
+                     coffee-mode
+                     web-mode
+                     projectile
+                     projectile-rails
+                     bundler
+                     git-commit
+                     gitconfig-mode
+                     gitignore-mode
+                     solarized-theme
+                     exec-path-from-shell))
+(eval-when-compile
+  (require 'cl))
+
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;;setup erc
+(when (package-installed-p 'erc)
+  (setq erc-hide-list '("JOIN" "PART" "QUIT")))
+
+;;setup ruby-mode
+(when (package-installed-p 'ruby-mode)
+  (add-to-list 'auto-mode-alist '("Procfile\\'" . ruby-mode)))
+
+;;setup css-mode
+(when (package-installed-p 'css-mode)
+  (setq css-indent-offset 2))
+
+;;setup js-mode
+(when (package-installed-p 'js)
+  (setq js-indent-level 2))
+
+;;setup projectile
+(when (package-installed-p 'projectile)
+  (setq projectile-completion-system 'default)
+  (projectile-global-mode))
+
+;;setup projectile-rails
+(when (package-installed-p 'projectile-rails)
+  (add-hook 'projectile-mode-hook 'projectile-rails-on))
+
+;;setup web-mode
+(when (package-installed-p 'web-mode)
+  (setq web-mode-markup-indent-offset 2)
+  (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode)))
+
+;;setup markdown-mode
+(when (package-installed-p 'markdown-mode)
+  (add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
+  (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode)))
+
+;;setup git-commit
+(when (package-installed-p 'git-commit)
+  (global-git-commit-mode t))
+
+;;setup window systems
+(when window-system
+  ;;setup scroll wheel for one line at a time
+  (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+  (setq mouse-wheel-progressive-speed nil)
+  (setq mouse-wheel-follow-mouse 't)
+  ;;setup solarized
+  (when (package-installed-p 'solarized-theme)
+    (load-theme 'solarized-dark t)))
+
+;;setup mac
 (when (memq window-system '(mac ns))
-  (require 'exec-path-from-shell)
-  (exec-path-from-shell-initialize))
-
-;git
-(add-to-list 'load-path "~/.emacs.d/vendor/git-modes")
-(require 'git-commit-mode)
-(require 'gitconfig-mode)
-(require 'gitignore-mode)
-
-;;markdown
-(add-to-list 'load-path "~/.emacs.d/vendor/markdown-mode")
-(autoload 'markdown-mode "markdown-mode" 
-  "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
-
+  ;;we hates popups
+  (setq ns-pop-up-frames nil)
+  ;;we likes emoji
+  (set-fontset-font t 'symbol (font-spec :family "Apple Color Emoji") nil 'prepend)
+  ;;setup exec-path-from-shell
+  (when (package-installed-p 'exec-path-from-shell)
+    (exec-path-from-shell-initialize)))
